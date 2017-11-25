6fe11699b604938c23b00b0114ca87e0854e53eb
Nicola Watts <nicola.d.watts@gmail.com>
2017-11-25 13:40:58 -0800

    NWatts update LED pattern

--- src/canwheel.c
+++ src/canwheel.c
@@ -27,6 +27,12 @@ volatile uint8_t cw_input_trigger = 0;
 
 volatile uint16_t rpm = 0xFFFF;
 volatile uint8_t cal = 0;
+volatile uint8_t flag = 0;
+volatile uint8_t i = 0;
+volatile uint16_t random = 0xFFFF;
+volatile uint16_t rpmVal[243] = {9929,9562,9342,9109,9073,8902,7678,4394,5066,5661,6118,6684,7038,7272,7549,7709,7654,7105,8283,7692,8223,8016,7896,7402,7255,6811,7123,7331,7890,8341,8239,7516,7193,6699,6653,6460,6904,7592,7269,8099,8475,8722,9216,9703,9841,10208,10700,10766,9959,9097,8462,7899,7141,6573,6531,6370,6645,6629,6821,6532,6260,6315,5944,6295,6585,6680,6906,7027,7569,8046,8068,7639,7004,6903,6597,7392,7068,6929,7777,8028,8342,8884,8929,7992,7687,7308,6730,6696,6384,6208,6114,7243,7887,9222,8842,9663,8482,8999,9548,9525,8585,8010,7057,7575,8822,8600,8848,9352,9044,7917,7272,7498,8649,8469,8587,8658,8560,8647,9341,9797,10073,10422,10839,11233,11781,12021,12454,12632,11851,11027,10712,9839,8345,6933,6496,6007,5252,4890,4761,6845,7118,5944,5892,6089,6254,5379,5130,4820,4625,4477,4858,5524,6365,5829,6160,6571,7107,7701,8204,8188,7815,7376,6741,6379,6015,5880,6369,5688,5832,5452,6241,6613,6574,6790,7375,7925,8249,8431,8659,9530,9230,9473,9715,9607,10058,10232,10297,9775,9458,9137,9806,10603,10109,9879,10103,10609,10759,10788,11168,10418,10068,9223,8690,7956,7304,6735,6756,6261,6478,6469,7199,7672,7970,8231,8225,7733,7018,8036,7602,8607,9195,9525,9649,9455,9876,9887,9831,9521,10906,11013,10712,11335,11819,12377,12313,12601,12985,13071,12916,12985,13024,12974,13024}; //1KB
+
+
 
 void cw_timer_init(void);
 
@@ -58,70 +64,59 @@ main(void)
 	
 	/* Enable global interrupts */
 	sei();
+	//if button is pressed and flag  = 0
 	
-	for (;;) {
-		
-		/* Perform periodic input tasks */
-		if (cw_adc_trigger) {
-			cw_adc_update();
-			cw_adc_trigger = 0;
-		}
-		
-		if (cw_input_trigger) {
-			cw_io_update();
-			cw_input_trigger = 0;
-		}
+	while(cw_io_flags(2) ==1){
+		rpm = 0;
+		// create rpm array, define size []
+		//rpmVal[2048] = [,,,,,]
+		//make unsigned int
+		for (i = 0; i <= 243; i++) {
+			rpm = rpmVal[i];
+				/* Update shift LEDs */
+				if (rpm == 0) {
+					cw_leds_set_off(1);
+					cw_leds_update();
 
-		if (cal == 2) {
-			cw_leds_set_amber(1);
-			cw_leds_set_green(0);
-		} else if (cal == 3) {
-			cw_leds_set_amber(0);
-			cw_leds_set_green(1);
-		} else if (cal == 4) {
-			cw_leds_set_amber(1);
-			cw_leds_set_green(1);
-		} else {
-			cw_leds_set_amber(0);
-			cw_leds_set_green(0);
-		}
-		
-		/* Update shift LEDs */
-		if (rpm == 0) {
-			cw_leds_set_off(1);
-		} else {
-			cw_leds_set_off(0);
-			
-			if (rpm >= 12500) {
-				cw_leds_set_blink(1);
-			} else {
-				cw_leds_set_blink(0);
+				} 
+				else {
+					cw_leds_set_off(0);
+					
+					if (rpm >= 12500) {
+						cw_leds_set_blink(1);
+					} 
+
+					else {
+						cw_leds_set_blink(0);
+						
+						if      (rpm >= 12000) cw_leds_set_shift(12);
+						else if (rpm >= 11000) cw_leds_set_shift(11);
+						else if (rpm >= 10000) cw_leds_set_shift(10);
+						else if (rpm >= 9000)  cw_leds_set_shift(9);
+						else if (rpm >= 8000)  cw_leds_set_shift(8);
+						else if (rpm >= 7000)  cw_leds_set_shift(7);
+						else if (rpm >= 6000)  cw_leds_set_shift(6);
+						else if (rpm >= 5000)  cw_leds_set_shift(5);
+						else if (rpm >= 4000)  cw_leds_set_shift(4);
+						else if (rpm >= 3000)  cw_leds_set_shift(3);
+						else if (rpm >= 2000)  cw_leds_set_shift(2);
+						else if (rpm >= 1000)  cw_leds_set_shift(1);
+					}
+				}
 				
-				if      (rpm >= 12000) cw_leds_set_shift(12);
-				else if (rpm >= 11000) cw_leds_set_shift(11);
-				else if (rpm >= 10000) cw_leds_set_shift(10);
-				else if (rpm >= 9000)  cw_leds_set_shift(9);
-				else if (rpm >= 8000)  cw_leds_set_shift(8);
-				else if (rpm >= 7000)  cw_leds_set_shift(7);
-				else if (rpm >= 6000)  cw_leds_set_shift(6);
-				else if (rpm >= 5000)  cw_leds_set_shift(5);
-				else if (rpm >= 4000)  cw_leds_set_shift(4);
-				else if (rpm >= 3000)  cw_leds_set_shift(3);
-				else if (rpm >= 2000)  cw_leds_set_shift(2);
-				else if (rpm >= 1000)  cw_leds_set_shift(1);
+				cw_leds_update();
+				_delay_ms(5);
+
+			
+			if(rpm >= 12000){
+				rpm = 0;
 			}
+
 		}
 		
-		cw_leds_update();
-		
-		/* Perform periodic output tasks */
-		if (cw_can_trigger) {
-			cw_can_send_inputs();
-			cw_can_trigger = 0;
-		}
 	}
 	
-	return 0;
+	//return 0;
 }
 
 void
--- src/cw_io.c
+++ src/cw_io.c
@@ -8,6 +8,8 @@
 #include "cw_io.h"
 
 uint8_t cw_io_values = 0;
+uint8_t flag2 = 0;
+uint8_t flag7 = 0;
 
 void
 cw_io_init(void)
@@ -24,9 +26,14 @@ cw_io_update(void)
 	if (PINC & (1 << PINC5)) cw_io_values &= ~0x01;
 	else cw_io_values |= 0x01;
 	
-	if (PINC & (1 << PINC6)) cw_io_values |= 0x02;
-	else cw_io_values &= ~0x02;
-	
+	if (PINC & (1 << PINC6)){
+		cw_io_values |= 0x02;
+		flag2 = 1;
+	}
+	else{ 
+		cw_io_values &= ~0x02;
+		//flag2 = 0;
+	}
 	if (PINB & (1 << PINB3)) cw_io_values |= 0x04;
 	else cw_io_values &= ~0x04;
 	
@@ -39,9 +46,14 @@ cw_io_update(void)
 	if (PINB & (1 << PINB5)) cw_io_values |= 0x20;
 	else cw_io_values &= ~0x20;
 	
-	if (PIND & (1 << PIND0)) cw_io_values |= 0x40;
-	else cw_io_values &= ~0x40;
-	
+	if (PIND & (1 << PIND0)){
+		cw_io_values |= 0x40;
+		flag7 = 1;
+	}
+	else {
+		cw_io_values &= ~0x40;
+		//flag7 = 0;
+	}
 	if (PINC & (1 << PINC0)) cw_io_values &= ~0x80;
 	else cw_io_values |= 0x80;
 }
@@ -50,4 +62,15 @@ uint8_t
 cw_io_value(uint8_t input)
 {
 	return cw_io_values & (1 << (input - 1));
+}
+
+uint8_t
+cw_io_flags(uint8_t flag)
+{
+	if (flag == 2){
+		return flag2;
+	}
+	if (flag ==7){
+		return flag7;
+	}
 }
